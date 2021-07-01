248c78bbf4d3d6b2f94911d82d11a50cc8a4d52c
vakumar0 <kumarvaibhav1602@gmail.com>
2021-07-01 23:34:01 +0530

    updated application name in package.json

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express();
 const port = 8000;
 const path = require('path');
 
-//import routes
+
+app.get('/favicon.ico', (req, res) => res.status(204));
 var apiRoutes = require('./api/routes/servicesRoutes');
 
 app.use(cors());
@@ -19,6 +20,16 @@ app.get("/*", function (req, res) {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  console.log(err.message)
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
